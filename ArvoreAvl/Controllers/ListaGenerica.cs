7e4d000b74e0758baa5c036a7f1329e29bcd0427
Leonardo Fronza <lfronza@gvdasa.com.br>
2023-09-15 20:03:21 -0300

    Add project files.

---
+++ ArvoreAvl/Controllers/ListaGenerica.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ArvoreAvl.Controllers
+{
+    public class ListaGenerica<T>
+    {
+
+        public ICollection<T> lista(IList<T> teste)
+        {
+            return teste;
+        }
+    }
+}
