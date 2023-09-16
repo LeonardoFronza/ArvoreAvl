ac2b6fc1630ac53ba0ca2de793408182f9c8f425
Leonardo Fronza <lfronza@gvdasa.com.br>
2023-09-15 21:36:12 -0300

    Create  basics itens in tree.

---
+++ ArvoreAvl/src/Controllers/NodeController.cs
@@ -0,0 +1,49 @@
+﻿
+using ArvoreAvl.src.Dtos;
+
+namespace ArvoreAvl.src.Controllers
+{
+    public class NodeController
+    {
+        public NodeController()
+        {
+            //newRoot();
+        }
+
+        Node? root = new Node();
+        Node newNode = new Node();
+        
+        ///// <summary>
+        ///// Criando a raiz da arvore.
+        ///// </summary>
+        //public void newRoot()
+        //{
+        //    root = null;
+        //}
+
+        public void Inserir(int number)
+        {
+            CreateRoot(number);
+
+            if(root.Id > number)
+            {
+                root.Right.Validator3000(number);
+            }
+
+            if(newNode.Id < number)
+            {
+                root.Left.Validator3000(number);
+            }
+        }
+
+        private void CreateRoot(int number)
+        {
+            newNode.Id = number;
+
+            if (root is null)
+            {
+                root = newNode;
+            }
+        }
+    }
+}
---
+++ ArvoreAvl/src/Dtos/Node.cs
@@ -0,0 +1,68 @@
+﻿
+namespace ArvoreAvl.src.Dtos
+{
+    public class Node
+    {
+        public Node() { }
+        public Node(int number)
+        {
+            Id = number;
+        }
+
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Esquerda.
+        /// </summary>
+        public Node? Left { get; set; }
+
+        /// <summary>
+        /// Direita.
+        /// </summary>
+        public Node? Right { get; set; }
+
+        /// <summary>
+        /// Fator de balanceamento.
+        /// </summary>
+        public int BFactor { get; set; }
+
+        /// <summary>
+        /// Valida os itens a serem inseridos.
+        /// </summary>
+        /// <param name="number"></param>
+        public void Validator3000(int number)
+        {
+            if (Id == number)
+            {
+                return;
+            }
+
+            if (Id > number)
+            {
+                if (Left == null)
+                {
+                    Left = new Node(number);
+                }
+                else
+                {
+                    Left.Validator3000(number);
+                }
+            }
+
+            if (Id < number)
+            {
+                if (Right == null)
+                {
+                    Right = new Node(number);
+                }
+                else
+                {
+                    Right.Validator3000(number);
+                }
+            }
+        }
+    }
+}
